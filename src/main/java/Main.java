b17718887a14141d7c92a8abdb63a55f4b822205
Akhmad Bakir <akhmgazbak@gmail.com>
2024-12-21 00:47:39 +0300

    Create some methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,58 @@
+
 public class Main {
     public static void main(String[] args) {
 
-        Manager manager = new Manager();
+        Manager task1 = new Manager();
 
-        manager.createTask("Сделать домашку", "Сделать всю домашку до воскресенья", TaskStatus.NEW);
+        task1.createTask("Сделать домашку", "Сделать всю домашку до воскресенья", TaskStatus.NEW);
 
-        System.out.println(manager.getTasksById(1));
+        System.out.println(task1.getTaskById(1));
 
-        manager.updateTasksById(1, "Сделать домашку", "Сделать всю домашку до воскресенья", TaskStatus.DONE);
+        task1.updateTaskById(1, "Сделать домашку", "Сделать всю домашку до воскресенья", TaskStatus.DONE);
 
-        System.out.println(manager.task.toString());
+        System.out.println(task1.getTaskById(1));
 
-        manager.createTask("Сходить в магазин", "Купить еду", TaskStatus.NEW);
+        Manager task2 = new Manager();
 
-        System.out.println(manager.task.toString());
+        task2.createTask("Сходить в магазин", "Купить еду", TaskStatus.NEW);
 
-        System.out.println(manager.getTasksById(2));
+        System.out.println(task2.getTaskById(1).toString());
 
-        manager.deleteTaskById(2);
+        task2.createTask("Сходить в кино", "Купить билеты", TaskStatus.NEW);
 
-        System.out.println(manager.task.toString());
+        System.out.println(task2.getTask());
+
+        task2.deleteTaskById(2);
+
+        System.out.println(task2.getTask());
+
+        task2.updateTaskById(1, "Сходил в кино", "Купил билеты", TaskStatus.DONE);
+
+        System.out.println(task2.getTask());
+
+        Manager subTasksHouseBuilder = new Manager();
+
+        subTasksHouseBuilder.createSubTask("Сделать фундамент", "Фундамент из бетона", TaskStatus.IN_PROGRESS);
+
+        subTasksHouseBuilder.addToSubTask("Сделать стены", "Стены из досок", TaskStatus.IN_PROGRESS);
+
+        subTasksHouseBuilder.addToSubTask("Сделать крышу", "Крыша из металла", TaskStatus.IN_PROGRESS);
+
+        Manager epicHouseBuilder = new Manager();
+
+        epicHouseBuilder.createEpic("Построить дом", "Дом должен быть большим", TaskStatus.IN_PROGRESS, subTasksHouseBuilder);
+
+        System.out.println(epicHouseBuilder.getEpic());
+
+        System.out.println(epicHouseBuilder.getEpicById(1));
+
+        System.out.println(epicHouseBuilder.getSubTasksInEpic(1)); //Выдает ссылку на объект класса Manager
+
+        subTasksHouseBuilder.updateSubTaskById(1,"Сделать фундамент", "Фундамент из бетона", TaskStatus.DONE);
+
+        epicHouseBuilder.deleteAllTasks();
+
+        System.out.println(epicHouseBuilder.getEpic());
 
 
     }
