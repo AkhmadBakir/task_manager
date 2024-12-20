2bf8d5c14a4e891ba1ca752a7d245d4039d8b5f0
Akhmad Bakir <akhmgazbak@gmail.com>
2024-12-20 07:29:44 +0300

    Create some methods

--- src/main/java/Epic.java
+++ src/main/java/Epic.java
@@ -1,6 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Epic extends Task {
 
     public Epic(Integer id, String name, String description, TaskStatus status) {
         super(id, name, description, status);
+
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        createTask()
+
+        Manager manager = new Manager();
+
+        manager.createTask("Сделать домашку", "Сделать всю домашку до воскресенья", TaskStatus.NEW);
+
+        System.out.println(manager.getTasksById(1));
+
+        manager.updateTasksById(1, "Сделать домашку", "Сделать всю домашку до воскресенья", TaskStatus.DONE);
+
+        System.out.println(manager.task.toString());
+
+        manager.createTask("Сходить в магазин", "Купить еду", TaskStatus.NEW);
+
+        System.out.println(manager.task.toString());
+
+        System.out.println(manager.getTasksById(2));
+
+        manager.deleteTaskById(2);
+
+        System.out.println(manager.task.toString());
+
+
     }
 }
